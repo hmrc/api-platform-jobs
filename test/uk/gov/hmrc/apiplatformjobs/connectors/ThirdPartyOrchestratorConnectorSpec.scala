783adcb7ebf54f01907cfb918a034a3856942a66
Andy Spaven <16537059+AndySpaven@users.noreply.github.com>
2025-11-20 08:48:48 +0000

    API-8761 - Migrate to OQE phase 2

--- test/uk/gov/hmrc/apiplatformjobs/connectors/ThirdPartyOrchestratorConnectorSpec.scala
+++ test/uk/gov/hmrc/apiplatformjobs/connectors/ThirdPartyOrchestratorConnectorSpec.scala
@@ -18,7 +18,6 @@ package uk.gov.hmrc.apiplatformjobs.connectors
 
 import java.time.format.DateTimeFormatter
 import java.time.temporal.ChronoUnit.DAYS
-import java.time.{LocalDateTime, ZoneOffset}
 import scala.concurrent.ExecutionContext.Implicits.global
 
 import com.github.tomakehurst.wiremock.client.WireMock._
@@ -27,7 +26,6 @@ import org.scalatestplus.play.guice.GuiceOneAppPerSuite
 import play.api.Application
 import play.api.http.Status._
 import play.api.inject.guice.GuiceApplicationBuilder
-import play.api.libs.json.Json
 import uk.gov.hmrc.http._
 import uk.gov.hmrc.http.client.HttpClientV2
 
@@ -75,7 +73,9 @@ class ThirdPartyOrchestratorConnectorSpec
 
     "return application Ids" in new Setup {
       stubFor(
-        get(urlPathEqualTo(s"/developer/${adminOne.userId}/applications"))
+        get(urlPathEqualTo("/query"))
+          .withQueryParam("userId", equalTo(s"${adminOne.userId}"))
+          .withQueryParam("status", equalTo("EXCLUDING_DELETED"))
           .willReturn(
             aResponse()
               .withStatus(OK)
@@ -91,30 +91,32 @@ class ThirdPartyOrchestratorConnectorSpec
 
     "propagate error when endpoint returns error" in new Setup {
       stubFor(
-        get(urlPathEqualTo(s"/developer/${adminOne.userId}/applications"))
+        get(urlPathEqualTo("/query"))
+          .withQueryParam("userId", equalTo(s"${adminOne.userId}"))
+          .withQueryParam("status", equalTo("EXCLUDING_DELETED"))
           .willReturn(
             aResponse()
-              .withStatus(NOT_FOUND)
+              .withStatus(499)
           )
       )
 
       intercept[UpstreamErrorResponse] {
         await(connector.fetchApplicationsByUserId(adminOne.userId))
-      }.statusCode shouldBe NOT_FOUND
+      }.statusCode shouldBe 499
     }
   }
 
   "applicationsLastUsedBefore" should {
-    def paginatedResponse(lastUseDates: List[ApplicationWithCollaborators]) =
-      PaginatedApplications(lastUseDates, 1, 100, lastUseDates.size, lastUseDates.size)
+    def response(apps: List[ApplicationWithCollaborators]) = apps
 
-    val dateFormatter: DateTimeFormatter = DateTimeFormatter.ISO_DATE_TIME
+    val dateFormatter: DateTimeFormatter = DateTimeFormatter.ISO_INSTANT
     val deleteRestriction                = DeleteRestrictionType.NO_RESTRICTION
 
     "return application details as ApplicationUsageDetails objects" in new Setup {
-      val lastUseLDT         = now.minusMonths(12)
-      val lastUseDate        = lastUseLDT.asInstant
-      val dateString: String = dateFormatter.format(lastUseLDT)
+      val checkLastUseInstant = now.minusMonths(12).asInstant
+      val createdInstant      = now.minusMonths(24).asInstant
+
+      val dateString: String = dateFormatter.format(checkLastUseInstant)
 
       val oldApplication1Admin = "foo@bar.com".toLaxEmail
       val oldApplication1      = standardApp.withCollaborators(
@@ -123,8 +125,8 @@ class ThirdPartyOrchestratorConnectorSpec
       )
         .modify(
           _.copy(
-            createdOn = LocalDateTime.now.minusMonths(12).toInstant(ZoneOffset.UTC),
-            lastAccess = Some(LocalDateTime.now.minusMonths(13).toInstant(ZoneOffset.UTC))
+            createdOn = createdInstant,
+            lastAccess = Some(now.minusMonths(13).asInstant)
           )
         )
       val oldApplication2Admin = "bar@baz.com".toLaxEmail
@@ -135,24 +137,25 @@ class ThirdPartyOrchestratorConnectorSpec
       )
         .modify(
           _.copy(
-            createdOn = LocalDateTime.now.minusMonths(12).toInstant(ZoneOffset.UTC),
-            lastAccess = Some(LocalDateTime.now.minusMonths(14).toInstant(ZoneOffset.UTC))
+            createdOn = createdInstant,
+            lastAccess = Some(now.minusMonths(14).asInstant)
           )
         )
 
       stubFor(
-        get(urlPathEqualTo("/environment/PRODUCTION/applications"))
-          .withQueryParam("lastUseBefore", equalTo(dateString))
+        get(urlPathEqualTo("/query"))
+          .withQueryParam("environment", equalTo("PRODUCTION"))
+          .withQueryParam("limit", equalTo("100"))
+          .withQueryParam("lastUsedBefore", equalTo(dateString))
           .withQueryParam("deleteRestriction", equalTo(deleteRestriction.toString))
-          .withQueryParam("sort", equalTo("NO_SORT"))
           .willReturn(
             aResponse()
               .withStatus(OK)
-              .withJsonBody(paginatedResponse(List(oldApplication1, oldApplication2)))
+              .withJsonBody(response(List(oldApplication1, oldApplication2)))
           )
       )
 
-      val results = await(connector.applicationSearch(Environment.PRODUCTION, Some(lastUseDate), deleteRestriction))
+      val results = await(connector.applicationSearch(Environment.PRODUCTION, Some(checkLastUseInstant), deleteRestriction))
 
       results should contain
       ApplicationUsageDetails(oldApplication1.id, oldApplication1.name, Set(oldApplication1Admin), oldApplication1.details.createdOn, oldApplication1.details.lastAccess)
@@ -161,36 +164,38 @@ class ThirdPartyOrchestratorConnectorSpec
     }
 
     "return empty Sequence when no results are returned" in new Setup {
-      val lastUseLDT         = now.minusMonths(12)
-      val lastUseDate        = lastUseLDT.asInstant
-      val dateString: String = dateFormatter.format(lastUseLDT)
+      val lastUseInstant     = now.minusMonths(12).asInstant
+      val dateString: String = dateFormatter.format(lastUseInstant)
 
       stubFor(
-        get(urlPathEqualTo("/environment/PRODUCTION/applications"))
-          .withQueryParam("lastUseBefore", equalTo(dateString))
+        get(urlPathEqualTo("/query"))
+          .withQueryParam("environment", equalTo("PRODUCTION"))
+          .withQueryParam("limit", equalTo("100"))
+          .withQueryParam("lastUsedBefore", equalTo(dateString))
           .withQueryParam("deleteRestriction", equalTo(deleteRestriction.toString))
-          .withQueryParam("sort", equalTo("NO_SORT"))
           .willReturn(
             aResponse()
               .withStatus(OK)
-              .withJsonBody(paginatedResponse(List.empty))
+              .withJsonBody(response(Nil))
           )
       )
 
-      val results = await(connector.applicationSearch(Environment.PRODUCTION, Some(lastUseDate), deleteRestriction))
+      val results = await(connector.applicationSearch(Environment.PRODUCTION, Some(lastUseInstant), deleteRestriction))
 
       results.size should be(0)
     }
 
     "ensure lastUseBefore is not in query param when lastUseDate is None" in new Setup {
       stubFor(
-        get(urlPathEqualTo("/environment/PRODUCTION/applications"))
+        get(urlPathEqualTo("/query"))
+          .withQueryParam("environment", equalTo("PRODUCTION"))
+          .withQueryParam("limit", equalTo("100"))
+          .withQueryParam("lastUsedBefore", absent())
           .withQueryParam("deleteRestriction", equalTo(deleteRestriction.toString))
-          .withQueryParam("sort", equalTo("NO_SORT"))
           .willReturn(
             aResponse()
               .withStatus(OK)
-              .withJsonBody(paginatedResponse(List.empty))
+              .withJsonBody(response(Nil))
           )
       )
 
@@ -203,50 +208,23 @@ class ThirdPartyOrchestratorConnectorSpec
   "toDomain" should {
     import uk.gov.hmrc.apiplatformjobs.connectors.ThirdPartyOrchestratorConnector.toDomain
 
-    "correctly convert ApplicationLastUseDates to ApplicationUsageDetails" in new PaginatedTPAResponse {
-      val parsedResponse: PaginatedApplications = Json.fromJson[PaginatedApplications](Json.parse(response)).get
+    "correctly convert ApplicationLastUseDates to ApplicationUsageDetails" in {
+      val createdOn  = now.minusYears(1).asInstant
+      val lastAccess = createdOn.plus(5, DAYS)
+      val apps       = List(standardApp.modify(_.copy(createdOn = createdOn, lastAccess = Some(lastAccess))))
 
-      val convertedApplicationDetails = toDomain(parsedResponse.applications)
+      val convertedApplicationDetails = toDomain(apps)
 
       convertedApplicationDetails.size should be(1)
       val convertedApplication = convertedApplicationDetails.head
 
-      convertedApplication.applicationId should be(applicationId)
-      convertedApplication.applicationName should be(applicationName)
+      convertedApplication.applicationId should be(standardApp.id)
+      convertedApplication.applicationName should be(standardApp.name)
       convertedApplication.creationDate should be(createdOn)
       convertedApplication.lastAccessDate should be(Some(lastAccess))
       convertedApplication.administrators.size should be(2)
-      convertedApplication.administrators should contain(adminEmailAddress)
-      convertedApplication.administrators should not contain (developerEmailAddress)
+      convertedApplication.administrators should contain(adminOne.emailAddress)
+      convertedApplication.administrators should not contain (developerOne.emailAddress)
     }
-  }
-
-  trait PaginatedTPAResponse {
-    val applicationId   = standardApp.id
-    val applicationName = standardApp.name
-    val createdOn       = now.minusYears(1).asInstant
-    val lastAccess      = createdOn.plus(5, DAYS)
-
-    val pageNumber           = 1
-    val pageSize             = 25
-    val totalApplications    = 500
-    val matchingApplications = 1
-
-    val adminEmailAddress     = standardApp.admins.head.emailAddress
-    val adminUserId           = standardApp.admins.head.userId
-    val developerEmailAddress = standardApp.collaborators.filter(_.isDeveloper).head.emailAddress
-    val developerUserId       = standardApp.collaborators.filter(_.isDeveloper).head.userId
-
-    val appJson = Json.toJson(standardApp.modify(_.copy(createdOn = createdOn, lastAccess = Some(lastAccess)))).toString()
-
-    val response = s"""{
-                      |  "applications": [
-                      |    $appJson
-                      |  ],
-                      |  "page": $pageNumber,
-                      |  "pageSize": $pageSize,
-                      |  "total": $totalApplications,
-                      |  "matching": $matchingApplications
-                      |}""".stripMargin
   }
 }
